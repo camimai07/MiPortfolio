4106c267ac286fb660242c279061d493d88c2912
camila <camilamaita07@gmail.com>
2024-06-29 13:10:07 -0300

    fix error wsp

--- src/Contact.js
+++ src/Contact.js
@@ -1,27 +1,50 @@
-export function Contact(){
-    return(
-        <footer>
-            <section id="Contact">
-                        
-                <div className="containerContact">
-                    <h1>Contacto</h1>
-                    <div className="icons">
-                        <div className="contenedor-a">
-                            <a href="https://wa.me/1169971238" target="_blank"><i className="fa-brands fa-whatsapp fa-2xl" style={{color: '#fffbfe'}}></i><br/>WhatsApp</a>
-                            
-                        </div>
-                        <div className="contenedor-a">
-                            <a href="mailto:camilamaita07@gmail.com" target="_blank"><i className="fa-solid fa-envelope g fa-2xl"style={{color: '#fffbfe'}}></i><br/>E-mail</a>
-                        </div>
-                        <div className="contenedor-a">
-                            <a href="https://drive.google.com/file/d/1deL5cWHjYUsaQc6gVG5u6ZUwWkR9hF2d/view?usp=sharing" target="_blank"><i className="fa-solid fa-sheet-plastic fa-2xl" style={{color: '#fffbfe'}}></i><br/>CV</a>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-                    
-            </section>
-        </footer>
-    );
+export function Contact() {
+  return (
+    <footer>
+      <section id="Contact">
+        <div className="containerContact">
+          <h1>Contacto</h1>
+          <div className="icons">
+            <div className="contenedor-a">
+              <a
+                href="https://wa.me/+5491169971238"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i
+                  className="fa-brands fa-whatsapp fa-2xl"
+                  style={{ color: "#fffbfe" }}
+                ></i>
+                <br />
+                WhatsApp
+              </a>
+            </div>
+            <div className="contenedor-a">
+              <a href="mailto:camilamaita07@gmail.com" target="_blank">
+                <i
+                  className="fa-solid fa-envelope g fa-2xl"
+                  style={{ color: "#fffbfe" }}
+                ></i>
+                <br />
+                E-mail
+              </a>
+            </div>
+            <div className="contenedor-a">
+              <a
+                href="https://drive.google.com/file/d/1deL5cWHjYUsaQc6gVG5u6ZUwWkR9hF2d/view?usp=sharing"
+                target="_blank"
+              >
+                <i
+                  className="fa-solid fa-sheet-plastic fa-2xl"
+                  style={{ color: "#fffbfe" }}
+                ></i>
+                <br />
+                CV
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,58 @@
-export function Home(){
+export function Home() {
+  return (
+    <>
+      <section id="Home">
+        <div className="containerHome">
+          <div className="borde left"></div>
+          <div className="img-texto">
+            <div className="texto">
+              <h1 className="animate__animated animate__lightSpeedInRight">
+                Camila Luciana Maita
+              </h1>
+              <br />
+              <h1>
+                Full Stack Developer<span className="guion">_</span>
+              </h1>
+              <br />
 
-
-    return(
-
-        <>
-            <section id="Home">
-                    <div className="containerHome">
-                        <div className="borde left"></div>
-                        <div className="img-texto">
-                            <div className="texto">
-                                <h1 className="animate__animated animate__lightSpeedInRight">Camila Luciana Maita</h1><br/>
-                                <h1 >Full Stack Developer<span className="guion">_</span></h1><br/>
-
-                                <div className="contacto-icons-home">
-                                    <div className="contacto-icons-home_a"><a href="https://wa.me/1169971238" target="_blank"><i className="fa-brands fa-whatsapp fa-lg" style={{color: '#fffbfe'}}></i></a></div>
-                                    <div className="contacto-icons-home_a"><a href="mailto:camilamaita07@gmail.com" target="_blank"><i className="fa-solid fa-envelope g"style={{color: '#fffbfe'}}></i></a></div>
-                                    <div className="contacto-icons-home_a"><a href="https://drive.google.com/file/d/1deL5cWHjYUsaQc6gVG5u6ZUwWkR9hF2d/view?usp=sharing" target="_blank"><i className="fa-solid fa-sheet-plastic" style={{color: '#fffbfe'}}></i></a></div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div className="borde right"></div>
-                    </div>
-                    
-
-                    
-            </section>
-        </>
-    );
+              <div className="contacto-icons-home">
+                <div className="contacto-icons-home_a">
+                  <a
+                    href="https://wa.me/+5491169971238"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i
+                      className="fa-brands fa-whatsapp fa-lg"
+                      style={{ color: "#fffbfe" }}
+                    ></i>
+                  </a>
+                </div>
+                <div className="contacto-icons-home_a">
+                  <a href="mailto:camilamaita07@gmail.com" target="_blank">
+                    <i
+                      className="fa-solid fa-envelope g"
+                      style={{ color: "#fffbfe" }}
+                    ></i>
+                  </a>
+                </div>
+                <div className="contacto-icons-home_a">
+                  <a
+                    href="https://drive.google.com/file/d/1deL5cWHjYUsaQc6gVG5u6ZUwWkR9hF2d/view?usp=sharing"
+                    target="_blank"
+                  >
+                    <i
+                      className="fa-solid fa-sheet-plastic"
+                      style={{ color: "#fffbfe" }}
+                    ></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="borde right"></div>
+        </div>
+      </section>
+    </>
+  );
 }
